eed13b1953640b2faf280b893dcfb297e5e867e2
Dawid Bazyluk <dawid.bazyluk@gmail.com>
2024-07-12 23:51:36 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,14 +3,28 @@ import ContactForm from "./ContactForm";
 import Filter from "./Filter";
 import ContactList from "./ContactList";
 
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { selectIsLoading, selectError } from "../redux/selectors";
+import { fetchContacts } from "../redux/contactsSlicer";
+
 const App = () => {
+  const dispatch = useDispatch();
+  const isLoading = useSelector(selectIsLoading);
+  const error = useSelector(selectError);
+
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
+
   return (
     <div>
       <h1>Phonebook</h1>
       <ContactForm />
       <h2>Contacts</h2>
       <Filter />
-      <ContactList />
+      {!!isLoading && !error && <b>Loading...</b>}
+      <ContactList></ContactList>
     </div>
   );
 };
